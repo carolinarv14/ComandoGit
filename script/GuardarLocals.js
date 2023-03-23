c5f3ad8edc6dba639a692ad4e54565d94db0be25
Carolina <carolinarinconv@iesantaelena.edu.co>
2023-03-23 16:13:57 -0500

    implementar cambios

--- script/GuardarLocals.js
+++ script/GuardarLocals.js
@@ -1,2 +1,56 @@
+
+
 function GuardarLocals(params) {
 }
+
+let vehiculo, modelo, tipo, placa, fecha, horas, cantidad_horas
+formulario.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    LeerDatos()
+    })
+    
+    function LeerDatos() {
+        vehiculo = document.getElementById('vehiculo').value
+        modelo = document.getElementById('modelo').value
+        tipo = document.getElementById ('tipo').value
+        fecha = document.getElementById ('fecha').value
+        horas = document.getElementById ('horas').value
+        cantidad_horas = document.getElementById ('cantidad_horas').value
+    
+
+        console.log(vehiculo)
+        ValidarData(vehiculo,modelo,tipo,fecha,horas,cantidad_horas)
+        GuardarLocalStorage (vehiculo,modelo,tipo,fecha,horas,cantidad_horas)
+    }
+    
+    function ValidarData(vehiculo, modelo, tipo, fecha, horas, cantidad_horas) {
+        if(vehiculo.length==0 || modelo.length==0 || tipo.length==0 || fecha.length==0 || horas.length==0 ||cantidad_horas.length==0 ||){
+    
+            Swal.fire({
+                title: 'Error!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })
+        }
+    }
+    
+    function GuardarLocalStorage(vehiculo,modelo,tipo,fecha,horas,cantidad_horas) {
+        localStorage.setItem('Vehiculo',vehiculo)
+        localStorage.setItem ('Modelo',modelo)
+        localStorage.setItem('Tipo',tipo)
+        localStorage.setItem('Fecha',fecha)
+        localStorage.setItem('Horas',horas)
+        localStorage.setItem('Cantidad_Horas',cantidad_horas)
+
+        ListarData()
+    }
+        function ListarData() {
+            let vehiculoUsu = localStorage.getItem('Vehiculo')
+            let modeloUsu = localStorage.getItem ('Modelo')
+            let tipoUsu = localStorage.getItem ('Tipo')
+            let fechaUsu = localStorage.getItem ('Fecha')
+            let horasUsu = localStorage.getItem ('Horas')
+            let cantidad_horasUsu = localStorage.getItem ('Cantidad_Horas')
+            
+        }
