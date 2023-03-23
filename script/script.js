f67dc01c3ea96eaeaa172d441d8c68213ca1064d
Carolina <carolinarinconv@iesantaelena.edu.co>
2023-03-23 14:30:11 -0500

    implementar javascript

--- script/script.js
+++ script/script.js
@@ -11,5 +11,31 @@ function LeerDatos() {
     nombre = document.getElementById('nombre').value
     correo = document.getElementById('correo').value
     mensaje = document.querySelector ('#textarea').value
-    console.log(nombre)
+    console.log(correo)
+    ValidarData(nombre,correo,mensaje)
+    GuardarLocalStorage (nombre,correo,mensaje)
 }
+
+function ValidarData(nombre,correo,mensaje) {
+    if(nombre.length==0 || correo.length==0 || mensaje.length==0){
+
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+    }
+}
+
+function GuardarLocalStorage(nombre,correo,mensaje) {
+    localStorage.setItem('Nombre',nombre)
+    localStorage.setItem ('Correo',correo)
+    localStorage.setItem('Mensaje',mensaje)
+    ListarData()
+}
+    function ListarData() {
+        let nombreUsu = localStorage.getItem('Nombre')
+        let correoUsu = localStorage.getItem ('Correo')
+        let mensajeUsu = localStorage.getItem('Mensaje')
+    }
